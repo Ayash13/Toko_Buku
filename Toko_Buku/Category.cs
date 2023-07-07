bdad59178a1b85e37a305bf178a80212020fadcb
Muhammad Ayash Al-fatih <m.ayashal.f@gmail.com>
2023-07-08 01:43:08 +0700

    Create LoadAdminData method to get admin data from admin table, also set the combo box admin

--- Toko_Buku/Category.cs
+++ Toko_Buku/Category.cs
@@ -23,6 +23,7 @@ namespace Toko_Buku
         {
             InitializeComponent();
             connection = new SqlConnection(connectionString);
+            LoadAdminData();
             LoadCategoryData();
         }
 
@@ -101,11 +102,40 @@ namespace Toko_Buku
 
         }
 
-        
+        private void LoadAdminData()
+        {
+            try
+            {
+                connection.Open();
+
+                string query = "SELECT AdminID, Username FROM Admin";
+                command = new SqlCommand(query, connection);
+                DataTable adminTable = new DataTable();
+
+                adapter = new SqlDataAdapter(command);
+                adapter.Fill(adminTable);
+
+                cbx_admin.DisplayMember = "Username";
+                cbx_admin.ValueMember = "AdminID";
+
+                cbx_admin.DataSource = adminTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
 
         private void cbx_admin_SelectedIndexChanged(object sender, EventArgs e)
         {
-           
+            if (cbx_admin.SelectedValue != null)
+            {
+                string adminID = cbx_admin.SelectedValue.ToString();
+            }
         }
     }
 }
