7618ae2fb2f6711749e2e6b66edf69be93cae1ee
Muhammad Ayash Al-fatih <m.ayashal.f@gmail.com>
2023-07-08 01:45:18 +0700

    create search function to search data by id and dynamically show it in gridview

--- Toko_Buku/Category.cs
+++ Toko_Buku/Category.cs
@@ -69,7 +69,32 @@ namespace Toko_Buku
 
         private void btn_search_Click(object sender, EventArgs e)
         {
-            
+            string query = "SELECT * FROM Category WHERE CategoryID = @idCat";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@idCat", txbx_search.Text);
+
+                    try
+                    {
+                        conn.Open();
+                        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        dataGridView1.DataSource = dataTable;
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message + " (Error Code: " + ex.Number + ")");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message);
+                    }
+                }
+            }
         }
 
         private void txbx_id_TextChanged(object sender, EventArgs e)
