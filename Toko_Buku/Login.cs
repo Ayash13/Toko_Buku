fd84a6aa878eff5d69d0e353d79df6ca16fce2c8
Muhammad Ayash Al-fatih <m.ayashal.f@gmail.com>
2023-07-10 03:14:07 +0700

    create function to check if input data = admin, then go to admin form, if input data = customers, then go to customers form

--- Toko_Buku/Login.cs
+++ Toko_Buku/Login.cs
@@ -27,19 +27,74 @@ namespace Toko_Buku
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void btn_masuk_Click(object sender, EventArgs e)
         {
+            string username = txbx_username.Text;
+            string password = txbx_password.Text;
 
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                // Admin
+                string adminQuery = "SELECT COUNT(*) FROM Admin WHERE Username = @Username AND Password = @Password";
+                using (SqlCommand adminCmd = new SqlCommand(adminQuery, conn))
+                {
+                    adminCmd.Parameters.AddWithValue("@Username", username);
+                    adminCmd.Parameters.AddWithValue("@Password", password);
+
+                    try
+                    {
+                        conn.Open();
+                        int adminResult = (int)adminCmd.ExecuteScalar();
+
+                        if (adminResult > 0)
+                        {
+                            form_menu menuAdmin = new form_menu();
+                            menuAdmin.Show();
+                            this.Hide();
+                            return;
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message + " (Error Code: " + ex.Number + ")");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message);
+                    }
+                }
+
+                // Customers
+                string userQuery = "SELECT COUNT(*) FROM Customer WHERE Name = @Name AND Password = @Password";
+                using (SqlCommand userCmd = new SqlCommand(userQuery, conn))
+                {
+                    userCmd.Parameters.AddWithValue("@Name", username);
+                    userCmd.Parameters.AddWithValue("@Password", password);
+
+                    try
+                    {
+                        int userResult = (int)userCmd.ExecuteScalar();
+
+                        if (userResult > 0)
+                        {
+                            CustomersForm customers = new CustomersForm();
+                            customers.Show();
+                            this.Hide();
+                            return;
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message + " (Error Code: " + ex.Number + ")");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message);
+                    }
+                }
+
+                MessageBox.Show("Invalid username or password. Please try again.");
+            }
         }
 
 
