e3701800e447a35a8a9bdcb8bcf75ce849811a2f
Muhammad Ayash Al-fatih <m.ayashal.f@gmail.com>
2023-07-09 21:49:19 +0700

    Create string connection for customers data

--- Toko_Buku/CustomersData.Designer.cs
+++ Toko_Buku/CustomersData.Designer.cs
@@ -126,6 +126,7 @@
             this.btn_delete.TabIndex = 69;
             this.btn_delete.Text = "Delete";
             this.btn_delete.UseVisualStyleBackColor = false;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
             // 
             // panel1
             // 
--- Toko_Buku/CustomersData.cs
+++ Toko_Buku/CustomersData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,19 @@ namespace Toko_Buku
 {
     public partial class CustomersData : Form
     {
+        private SqlConnection connection;
+        private SqlCommand command;
+
+        private string connectionString = "Data Source=DESKTOP-AJFQKR8\\AYASH;Initial Catalog=Toko_Buku;Persist Security Info=True;User ID=sa;Password=Rahasiatau123";
         public CustomersData()
         {
             InitializeComponent();
+            connection = new SqlConnection(connectionString);
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
