20aca3001087617e2fe852cf7f02d65135cbdbf6
Muhammad Ayash Al-fatih <m.ayashal.f@gmail.com>
2023-07-09 20:48:25 +0700

    create function to delete data by id ExpeditionID

--- Toko_Buku/Expedition.Designer.cs
+++ Toko_Buku/Expedition.Designer.cs
@@ -71,6 +71,7 @@
             this.btn_delete.TabIndex = 59;
             this.btn_delete.Text = "Delete";
             this.btn_delete.UseVisualStyleBackColor = false;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
             // 
             // label7
             // 
